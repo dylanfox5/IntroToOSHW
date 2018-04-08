6130327b98ae0c8cc1a0c9ad2d86c45eb42e5a31
Dylan Fox <dff1@graceland.edu>
2018-04-08 14:41:27 -0500

    website update

---
+++ inClass.py
@@ -0,0 +1,24 @@
+import os
+import time
+import sys
+import subprocess
+
+##print(os.getpid())
+##
+##print(os.getenv("Temp"))
+##
+##os.spawnl(os.P_NOWAIT, "c:\windows\write.exe", "write.exe")
+##
+##os.system("tree")
+
+print(sys.byteorder)
+
+print(sys.copyright)
+
+sys.exit(1)
+
+print(sys.getwindowsversion())
+
+print(sys.getrecursionlimit())
+
+
